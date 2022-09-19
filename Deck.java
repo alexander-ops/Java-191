66ba2dc4b3e9805baea85cce14943e52159fa267
alexander-ops <55400730+alexander-ops@users.noreply.github.com>
2022-09-18 21:04:15 -0700

    Create Deck.java

---
+++ Deck.java
@@ -0,0 +1,14 @@
+package ClashRoyale;
+
+public class Deck {
+    private int cardAmount;
+    private int maxCardAmount;
+    private int deckNumber;
+
+    public void addCard(){
+
+    }
+    public void removeCard(){
+
+    }
+}
