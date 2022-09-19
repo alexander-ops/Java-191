d294924d2f4557e261488ca8b6bb89ef827106ac
alexander-ops <55400730+alexander-ops@users.noreply.github.com>
2022-09-18 21:03:53 -0700

    Create Card.java

---
+++ Card.java
@@ -0,0 +1,13 @@
+package ClashRoyale;
+
+public class Card extends Deck{
+
+    private int health;
+    private int cardID;
+    private int damage;
+    private boolean attack;
+
+    public void useCard(){
+
+    }
+}
